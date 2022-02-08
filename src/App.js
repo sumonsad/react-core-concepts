2ce6cd0ddcb7041239342dbcfa82a2ac402abefa
sumonsad <sumonsad22518@gmail.com>
2022-02-08 16:32:54 +0600

    component added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = ['Bipul','Junayed','Abir','Raiyaan'];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <h1 style={{color:'yellow'}}>React Practice</h1>
+        <Person name = {name[0]} profession = "Student"></Person>
+        <Person name="Junayed" profession = "Student"></Person>
+        <Person name="Abir" profession = "Student"></Person>
+        <Person name = "Raiyaan" profession = "Cute Baby"></Person>
         
       </header>
     </div>
   );
+}
+
+function Person(props){
+  var personStyle = {
+    border:'2px solid red',
+    width:'400px',
+    margin:'5px',
+    padding:'5px'
+  }
+  return (
+  <div style= {personStyle}>
+    <h1>Name: {props.name}</h1>
+    <h3>Profession: {props.profession}</h3>
+  </div>
+  )
 }
 
 export default App;
